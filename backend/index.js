4152f3552216fec38901f0aa9c92dd6d9f4df46a
harshpratap2510 <harshpratapnitp@gmail.com>
2024-12-23 20:04:58 +0530

    remove env

--- backend/index.js
+++ backend/index.js
@@ -34,16 +34,16 @@ app.use("/api/v1/auth", authRoute);
 
  
 
-import { fileURLToPath } from 'url';
+// import { fileURLToPath } from 'url';
 
-// Get __dirname equivalent in ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// // Get __dirname equivalent in ES Modules
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-// Serve static files directly from the frontend folder (not `dist`)
-app.use(express.static(path.join(__dirname, 'frontend')));
+// // Serve static files directly from the frontend folder (not `dist`)
+// app.use(express.static(path.join(__dirname, 'frontend')));
 
-// For any other route, serve the index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
-});
+// // For any other route, serve the index.html
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+// });
