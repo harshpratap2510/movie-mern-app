7a35abb4ba0f986b8608fa6b57ee1e634cd81db3
harshpratap2510 <harshpratapnitp@gmail.com>
2025-01-09 22:47:34 +0530

    frontend url in cors

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-    origin: [process.env.FRONTEND_URL || 'https://movie-mern-c7kl4hamq-harsh-pratap-singh-s-projects.vercel.app'], 
+    origin: [process.env.FRONTEND_URL || 'https://movie-mern-app.vercel.app/'], 
     credentials: true
 }));
 
